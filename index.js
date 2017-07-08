42d756ec580468e99b6cc5e8a2b220451d0dcc65
Andrew Van Slaars <avanslaars@gmail.com>
2017-07-08 10:21:09 -0400

    add deps setup basic server and route to post command

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+const Hapi = require('hapi')
+
+const server = new Hapi.Server()
+
+server.connection({
+  host: 'localhost',
+  port: 8000
+})
+
+server.route([{
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    return reply('hello world')
+  }
+}, {
+  method: 'POST',
+  path: '/command',
+  handler: function (request, reply) {
+    console.log(request.payload)
+    reply('payload received')
+  }
+}])
+
+server.start((err) => {
+  if (err) {
+    throw err
+  }
+  console.log('Server running at:', server.info.uri)
+})
